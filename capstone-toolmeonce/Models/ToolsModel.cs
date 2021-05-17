0906785a48a3ff3ffae5f7d19168c1ef143f35fd
Braxton Shoop <braxton.shoop@gmail.com>
2021-05-17 18:24:32 -0500

    adding models completed

---
+++ capstone-toolmeonce/Models/ToolsModel.cs
@@ -0,0 +1,27 @@
+﻿namespace capstone_toolmeonce.Models
+{
+    public class ToolsModel
+    {
+        public int Id { get; set; }
+
+        public int LenderId { get; set; }
+
+        public string LenderEmail { get; set; }
+
+        public int BorrowerId { get; set; }
+
+        public string BorrowerEmail { get; set; }
+
+        public string ToolPicture { get; set; }
+
+        public int ToolAvailable { get; set; }
+
+        public string ToolName { get; set; }
+
+        public string ToolDescription { get; set; }
+
+        public string ToolSpecifications { get; set; }
+
+        public string ToolAccessories { get; set; }
+    }
+}
---
+++ capstone-toolmeonce/Models/UserProfileModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace capstone_toolmeonce.Models
+{
+    public class UserProfileModel
+    {
+        public int Id { get; set; }
+
+        public string FirebaseUserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime DateRegistered { get; set; }
+
+        public DateTime DateDeactivated { get; set; }
+
+        public DateTime DateLastActivated { get; set; }
+
+        public int IsActive { get; set; }
+    }
+}
